f3667538fe2ac28b2fddec6c19fe3f56e7ca1946
RRO (RSL-Md. Motiur Rahman) <RRO@rahimafrooz.com>
2022-11-01 18:01:29 +0600

    update code

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./Routes/Routes";
+import Home from "./Components/Home/Home/Home";
 
 function App() {
   return (
     <div data-theme="cupcake" className="max-w-7xl mx-auto">
       <RouterProvider router={router}></RouterProvider>
+      <Home></Home>
     </div>
   );
 }
